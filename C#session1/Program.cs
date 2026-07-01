2526012ba6bd7e969a493a2ef69139f1859f0135
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 07:27:47 +0400

    Add project files.

---
+++ C#session1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C_session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ahmed is a great programmer");
+        }
+    }
+}
