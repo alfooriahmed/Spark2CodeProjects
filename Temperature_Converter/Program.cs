782e7d656ab90f21265964da137e9f20a4aa8ab9
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 13:14:14 +0400

    added Task 6

---
+++ Temperature_Converter/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Temperature_Conventer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            Console.Write("Enter The Tempreture in celecis: ");
+            double temp = double.Parse(Console.ReadLine());
+            //Console.WriteLine(temp);
+
+
+            double feher = (temp * 9 / 5) + 32;
+            Console.WriteLine("The tempreture in fehrnhiet is: "+ feher);
+
+            if (temp < 10)
+            {
+                Console.WriteLine("Cold");
+            }
+
+            else if (temp >= 10 && temp <= 30)
+            {
+                Console.WriteLine("Mild");
+
+            }
+
+           /* else if (temp > 30)
+            {
+                Console.WriteLine("Hot");
+
+            } */
+
+            else
+            {
+                Console.WriteLine("Hot");
+            }
+
+    }
+
+
+
+}
+}
+
+
