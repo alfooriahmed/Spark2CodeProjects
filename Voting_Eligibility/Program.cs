b86bb472aeff7aa3554ac8b96c59ca7d3d9723ac
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 10:45:23 +0400

    added the fourth task

---
+++ Voting_Eligibility/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Voting_Eligibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine(age);
+
+            Console.Write("Do you have a vaild Nationl ID card (only Yes/ No)?");
+            string ID = Console.ReadLine();
+
+            if (age >= 18 && ID == "Yes" || ID == "yes" && age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+
+            else
+            {
+                Console.WriteLine("You are not eligible to vote");
+            }
+
+
+
+          
+            /*if( ID == true)
+            {
+                Console.WriteLine("yes he has an ID");
+            }  */  
+
+
+
+        }
+    }
+}
