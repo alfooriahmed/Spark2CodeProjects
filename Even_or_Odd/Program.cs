5dd8a4ec03f9679f1a8132996cf36acfb58097e9
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 09:29:29 +0400

    added the third task

---
+++ Even_or_Odd/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("please Enter a number: ");
+            double numb = double.Parse(Console.ReadLine());
+            if (numb % 2 == 0)
+            {
+                Console.WriteLine("the number is even");
+            }
+
+            else
+            {
+                Console.WriteLine("the number is odd");
+            }
+           
+
+        }
+    }
+}
