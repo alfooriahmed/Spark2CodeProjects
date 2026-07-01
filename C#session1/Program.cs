403e7763f0e1bf947c5ad5fb6c8460aebec96880
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 07:44:49 +0400

    added first addition code

--- C#session1/Program.cs
+++ C#session1/Program.cs
@@ -8,7 +8,10 @@
             Console.WriteLine("Ahmed is a great programmer");
             Console.WriteLine("Ahmed is a great programmer");
             Console.WriteLine("Ahmed is a great programmer");
-
+            int firstnum = 3;
+            int secondnum = 6;
+            int result = firstnum + secondnum;
+            Console.WriteLine(result);
         }
     }
 }
