7c76c03806b63455909d4c3784761bf41e4acc80
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 08:30:42 +0400

    added new file

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpSession2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ PersonalCard/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+namespace PersonalCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ahmed";
+            int age = 24;
+            double haight = 1.70;
+            bool student = true;
+
+            Console.WriteLine("Name: " +name);
+            Console.WriteLine("Age: " +age);
+           Console.WriteLine("Haight: " +haight);
+            Console.WriteLine("Student: "+student);
+
+
+
+
+        }
+
+    }
+}
