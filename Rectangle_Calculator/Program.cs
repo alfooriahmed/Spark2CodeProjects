1c53aa935c231546c02b4cfe2b425e7cd634a7cf
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 09:20:04 +0400

    add the secon task

---
+++ Rectangle_Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Rectangle_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the length: ");
+            int length = int.Parse(Console.ReadLine());
+
+            int area = length * width;
+            int perimeter = 2 * (length + width);
+
+            Console.WriteLine("The Area of the rectangle is: " + area + " and the permitr is: " + perimeter);
+            
+        }
+    }
+}
