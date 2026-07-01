410248420c8a479954b181b11461540b60c14d0d
Ahmed <alfooriahmed3@gmail.com>
2026-07-01 11:13:00 +0400

    added fifth task

---
+++ GradeLetterLookup/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace GradeLetterLookup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+           Console.Write("Please Enter Your Grade (A, B, C, D, F): ");
+            char grade = char.Parse(Console.ReadLine().ToUpper());
+            //Console.Write(grade);
+
+            switch(grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("Very Good");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+
+                case 'D':
+                    Console.WriteLine("Pass");
+                    break;
+
+                case 'F':
+                    Console.WriteLine("Fail");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Grade");
+                    break;
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
