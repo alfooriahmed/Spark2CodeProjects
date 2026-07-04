19a266ccec9bf1fe5b57a2bcc73b7b921461f369
Ahmed <alfooriahmed3@gmail.com>
2026-07-04 22:37:20 +0400

    finished Task 9 and 10

---
+++ Mini_Calculater/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Mini_Calculater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter The First Number: ");
+            int first_num = int.Parse(Console.ReadLine());
+            Console.Write("Please Enter The Second Number: ");
+            int second_num = int.Parse(Console.ReadLine());
+
+           // Console.WriteLine(first_num + second_num);
+
+            //Enter an operator
+            Console.Write("Please Enter an Operator: ");
+            char Operator = char.Parse(Console.ReadLine());
+           // Console.WriteLine(Operator);
+
+            switch (Operator)
+            {
+                case '+':
+                    Console.WriteLine(first_num + second_num);
+                    break;
+
+                case '-':
+                    Console.WriteLine(first_num - second_num);
+                    break;
+
+                case '*':
+                    Console.WriteLine(first_num * second_num);
+                    break;
+
+                case '/':
+                    if (second_num == 0)
+                    {
+                        Console.WriteLine("Can not divide by zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine(first_num / second_num);
+                    }
+                    break;
+
+                case '%':
+                    if (second_num == 0)
+                    {
+                        Console.WriteLine("Can not divide by zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine(first_num % second_num);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+
+
+
+            }
+        }
+    }
+}
