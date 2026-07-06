cb5f3e3c7c07b3dde8c87cbef63bb21dce17becf
Ahmed <alfooriahmed3@gmail.com>
2026-07-06 17:07:30 +0400

    Task3 (loops)

---
+++ Multiplication_Table/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number: ");
+            int user_num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine(user_num + " * " + i + " = " + user_num * i);
+                //Console.WriteLine(i);
+            }
+
+            //Console.WriteLine(user_num);
+        }
+    }
+}
