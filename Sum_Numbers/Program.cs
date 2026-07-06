a5194e90ebedfb3a3732175a6c502c56dfce5092
Ahmed <alfooriahmed3@gmail.com>
2026-07-06 15:18:24 +0400

    Task2 (loops)

---
+++ Sum_Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pleas Enter a Number: ");
+            int user_num = int.Parse(Console.ReadLine());
+            int total = 0;
+            for (int i = 0; i <= user_num; i += 1)
+            {
+                //Console.WriteLine(i);
+                total = total + i;
+
+            }
+            Console.WriteLine(total);
+
+
+           
+
+
+
+        }
+
+
+    }
+}
