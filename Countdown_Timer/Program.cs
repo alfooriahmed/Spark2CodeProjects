4c3b5eabcaec814dfa080dcf690e2694c9319457
Ahmed <alfooriahmed3@gmail.com>
2026-07-06 14:56:26 +0400

    Task1 (loops)

---
+++ Countdown_Timer/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Countdown_Timer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Number: ");
+            int user_num = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(user_num);
+            for(int i = 1;  user_num >= i; user_num--)
+            {
+                Console.WriteLine(user_num);
+            }
+            Console.WriteLine("Liftoff!");
+        }
+    }
+}
---
+++ Loan_System/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Loan_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter Your Montly Income: ");
+            int salary = int.Parse(Console.ReadLine());
+
+            Console.Write("Do You Have a Loan?(Yes/No) ");
+            string have_load = Console.ReadLine();
+            Console.WriteLine(have_load);
+
+
+            Console.WriteLine("your age is :" + age + " ,And your income is: " + salary);
+
+
+        }
+    }
+}
