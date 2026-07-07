a05f9c1176455ed6e6796a4d9e1364f748406461
Ahmed <alfooriahmed3@gmail.com>
2026-07-07 11:17:46 +0400

    Task7 (loops)

---
+++ Repeating_Menu/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Repeating_Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console.Write("Please Etner a Number: ");
+            Console.WriteLine("Please choose a number: ");
+            Console.WriteLine("1) hello");
+            Console.WriteLine("2) Good evening");
+            Console.WriteLine("3) Exist");
+            int num;
+            num = int.Parse(Console.ReadLine());
+            //Console.Write(num);
+
+            while (num != 3)
+            {
+                /* 
+                Console.WriteLine("Please choose a number: ");
+                Console.WriteLine("1) hello");
+                Console.WriteLine("2) Good evening");
+                Console.WriteLine("3) Exist"); 
+                
+                 */
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Hello!");
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("Good Evening!");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Exist");
+                        break;
+
+                }
+                Console.WriteLine("Please choose again: ");
+                Console.WriteLine("1) hello");
+                Console.WriteLine("2) Good evening");
+                Console.WriteLine("3) Exist");
+
+                //Console.Write("Please Enter the Correct Num: ");
+                num = int.Parse(Console.ReadLine());
+
+            }
+
+
+
+
+            Console.WriteLine("Nice, you got it..");
+
+
+        }
+    }
+}
