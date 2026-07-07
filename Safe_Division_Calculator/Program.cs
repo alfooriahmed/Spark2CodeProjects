6a729a20b32a089ab5a9f3aee5d48ed01a3feec1
Ahmed <alfooriahmed3@gmail.com>
2026-07-07 09:54:13 +0400

    Task6 (exception error)

---
+++ Safe_Division_Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Safe_Division_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            try
+            {
+                Console.Write("Pleas Enter The First Number: ");
+                int input1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Pleas Enter The Second Number: ");
+                int input2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(input1 / input2);
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Can't divide by zero: " + ex.Message);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("You Enter a not valid integer. Please enter numbers only.");
+            }
+
+            /*
+             *  Console.Write("Pleas Enter The First Number: ");
+                if(!int.TryParse(Console.ReadLine(), out int number)) 
+                {
+                    // message
+                    return
+                }
+                Console.Write(number);
+             */
+
+
+        }
+    }
+}
